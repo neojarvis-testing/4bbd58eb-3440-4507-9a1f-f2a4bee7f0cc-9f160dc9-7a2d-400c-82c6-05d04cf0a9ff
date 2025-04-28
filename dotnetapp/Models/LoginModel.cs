5090f59f2cf3eba709145470e46e3c18eb6b174b
Rohan Keshav Kanchan <134606727+rohan5255@users.noreply.github.com>
2025-04-28 16:09:20 +0530

    Update LoginModel.cs

--- dotnetapp/Models/LoginModel.cs
+++ dotnetapp/Models/LoginModel.cs
@@ -1,12 +1,1 @@
-namespace dotnetapp.Models
-{
-    // Represents the login credentials provided by the user
-    public class LoginModel
-    {
-        // Stores the user's email address for authentication
-        public string Email { get; set; }
 
-        // Stores the user's password for authentication
-        public string Password { get; set; }
-    }
-}
