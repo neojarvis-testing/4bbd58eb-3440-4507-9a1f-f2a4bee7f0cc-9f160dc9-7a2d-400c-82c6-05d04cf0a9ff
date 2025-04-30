198552416bbd9901c41f9ca855713c8a4f334c0b
ShivaniiSharmaa <shivanisharma09755@gmail.com>
2025-04-30 12:06:51 +0000

    just check

--- angularapp/src/app/services/auth.service.ts
+++ angularapp/src/app/services/auth.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { User } from '../models/user.model';
+import { Login } from '../models/login.model';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public baseUrl = 'https://8080-abcbddecbfceedecbfefbafcfdcadccdcfaff.premiumproject.examly.io/api';
+  private currentUserSubject: BehaviorSubject<User | null>;
+  public currentUser: Observable<User | null>;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    const storedUser = localStorage.getItem('currentUser');
+    this.currentUserSubject = new BehaviorSubject<User | null>(
+      storedUser ? JSON.parse(storedUser) : null
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User | null {
+    return this.currentUserSubject.value;
+  }
+
+  register(newUser: User): Observable<User> {
+    return this.http.post<User>(`${this.baseUrl}/register`, newUser);
+  }
+
+  login(loginData: Login): Observable<{ token: string; user: User }> {
+    return this.http.post<{ token: string; user: User }>(`${this.baseUrl}/login`, loginData).pipe(
+      tap(response => {
+        const { token, user } = response;
+        if (token && user) {
+          localStorage.setItem('jwtToken', token);
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+      })
+    );
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('jwtToken');
+  }
+
+  getUserRole(): string | null {
+    const user = this.currentUserValue;
+    return user ? user.UserRole : null;
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
 }
