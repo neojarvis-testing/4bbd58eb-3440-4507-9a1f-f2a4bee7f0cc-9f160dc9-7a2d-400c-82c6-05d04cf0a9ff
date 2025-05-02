cac4a5416ad6549ffb431c4ff8a3349e61cde7b5
ShivaniiSharmaa <shivanisharma09755@gmail.com>
2025-05-02 10:56:25 +0000

    admin-add-edit-viewTraining

--- angularapp/src/app/components/adminaddtraining/adminaddtraining.component.ts
+++ angularapp/src/app/components/adminaddtraining/adminaddtraining.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PhysicalTraining } from 'src/app/models/physical-training.model';
+import { PhysicalTrainingService } from 'src/app/services/physical-training.service';
 
 @Component({
   selector: 'app-adminaddtraining',
@@ -7,9 +12,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminaddtrainingComponent implements OnInit {
 
-  constructor() { }
-
+  newTraining : PhysicalTraining = {
+    TrainingName: '',
+    Description: '',
+    TrainerName: '',
+    Location: '',
+    IsIndoor: true,
+    Fee: 0,
+    FocusArea: '',
+    PhysicalRequirements: ''
+  }
+  formSubmitted: boolean = false;
+  trainings : PhysicalTraining[] = []
+  isTrainingExist : boolean = false;
+  isDialogOpen : boolean = false;
+  
+  constructor(private trainingService : PhysicalTrainingService, private router: Router) { }
+ 
+ 
   ngOnInit(): void {
+     this.loadTrainings();
+  }
+ 
+  loadTrainings() {
+    this.trainingService.getAllPhysicalTrainings().subscribe((data) => {
+      this.trainings = data;
+      console.log(this.trainings);
+    })
+  }
+ 
+  addTraining() {
+    this.formSubmitted = true;
+    console.log(`${this.formSubmitted} formsub`);
+    const isUnique = this.trainings.every(i => i.TrainingName.toLowerCase() != this.newTraining.TrainingName.toLowerCase())
+    console.log(isUnique);
+    
+    if(isUnique) {
+      if(this.IsFormValid()) {
+        this.trainingService.addPhysicalTraining(this.newTraining).subscribe(() => {
+          this.resetForm();
+        })
+        this.openDialog();
+      }
+    } else {
+      this.isTrainingExist = true;
+    }
   }
 
+  IsFormValid(): boolean{
+    if(this.newTraining.TrainerName &&  
+      this.newTraining.Description && 
+      this.newTraining.TrainerName && 
+      this.newTraining.Location && 
+      this.newTraining.Fee && 
+      this.newTraining.FocusArea && 
+      this.newTraining.PhysicalRequirements){
+        return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  resetForm(): void{
+    this.formSubmitted = false;
+    this.newTraining = {
+      TrainingName: '',
+      Description: '',
+      TrainerName: '',
+      Location: '',
+      IsIndoor: true,
+      Fee: 0,
+      FocusArea: '',
+      PhysicalRequirements: ''
+    }
+  }
+
+
+  openDialog() {
+    this.isDialogOpen = true;
+    document.body.classList.add('dialog-open');
+  }
+ 
+  closeDialog(): void {
+    this.isDialogOpen = false;
+    document.body.classList.remove('dialog-open');
+    this.router.navigate(['/view-trainings']);
+  }
 }
+
+
